bffd14c6e7e2dd83c185950849061c85186b4db6
Jeremy Adam <underway113@gmail.com>
2019-05-02 02:15:27 +0700

    Add min max avg Value

--- CheckAltitude/CheckAltitude/ViewController.swift
+++ CheckAltitude/CheckAltitude/ViewController.swift
@@ -11,38 +11,58 @@ import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    
+    //IBOutlet
     @IBOutlet weak var altitudeLabel: UILabel!
     @IBOutlet weak var minLabel: UILabel!
     @IBOutlet weak var maxLabel: UILabel!
+    @IBOutlet weak var avgLabel: UILabel!
     let manager = CLLocationManager()
     
-
+    //Variable
     var minValue:Double = 999999
     var maxValue:Double = 0
+    let historyCapacity = 10
     
+    var historyArray:[Double] = []
+    
+    //CoreLocation Function
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        var formattedMinValue:String = "0 m"
+        var formattedMaxValue:String = "0 m"
+        var formattedAvgValue:String = "0 m"
         let location: CLLocation? = locations[0] as CLLocation
         let altitudeValue = location?.altitude
         
-        if minValue > altitudeValue as! Double {
-//            lastValue = altitudeValue ?? 0
-            minValue = altitudeValue ?? 0
-            let formattedminValue = (NSString(format:"%.5f", minValue) as String) + " m"
-            minLabel.text = formattedminValue
-            
-        }
-        if maxValue < altitudeValue as! Double {
-//            lastValue = altitudeValue ?? 0
-            maxValue = altitudeValue ?? 0
-            let formattedmaxValue = (NSString(format:"%.5f", maxValue) as String) + " m"
-            maxLabel.text = formattedmaxValue
+        historyArray.append(altitudeValue ?? 0)
+        if historyArray.count > historyCapacity {
+            historyArray.removeFirst()
         }
         
-        let formattedAltitudeValue = (NSString(format:"%.5f", altitudeValue!) as String) + " m" as String
+        if minValue > altitudeValue as! Double {
+            minValue = altitudeValue ?? 0
+            formattedMinValue = (NSString(format:"%.4f", minValue) as String) + " m"
+            minLabel.text = formattedMinValue
+        }
+        
+        if maxValue < altitudeValue as! Double {
+            maxValue = altitudeValue ?? 0
+            formattedMaxValue = (NSString(format:"%.4f", maxValue) as String) + " m"
+            maxLabel.text = formattedMaxValue
+        }
+        
+        
+        let sumArray = historyArray.reduce(0, +)
+        let avgArrayValue = sumArray / Double(historyArray.count)
+        formattedAvgValue = (NSString(format:"%.4f", avgArrayValue) as String) + " m"
+        
+        let formattedAltitudeValue = (NSString(format:"%.4f", altitudeValue!) as String) + " m" as String
+        
         print("Current: \(formattedAltitudeValue)")
         altitudeLabel.text = formattedAltitudeValue
+        avgLabel.text = formattedAvgValue
     }
+    //
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -58,6 +78,8 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 manager.delegate = self
                 manager.desiredAccuracy = kCLLocationAccuracyBest
                 manager.startUpdatingLocation()
+            default:
+                print("Error Location")
             }
         } else {
             print("Location services are not enabled")
